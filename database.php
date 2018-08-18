cdf690540c0556949b2706144d56f4161d1875d2
Giovanny <giovankyandres@gmail.com>
2018-08-18 08:30:24 -0800

    Primer commit

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'proyecto';
+try {
+  $conexion = new PDO("mysql:host=$host;dbname=$database;", $username, $password);
+} catch (PDOException $e) {
+  die('Conexion Fallida: ' . $e->getMessage());
+}
